99b8154e3860929524d92c98df1e10c70ca31034
Deepanshu <deepanshusaxena315@gmail.com>
2025-05-30 23:23:54 +0530

    Initial commit

---
+++ src/components/ThemeToggle.jsx
@@ -0,0 +1,11 @@
+import { Bluetooth, Moon, Sun } from "lucide-react"
+
+export const ThemeToggle = () => {
+    const [isDarkMode, setIsDarkMode] = useState(false)
+  return (
+    <button>
+        {isDarkMode ? <Sun/>:<Moon/>}
+    </button>
+  )
+}
+
