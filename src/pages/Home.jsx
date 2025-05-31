1bf5b33250c1e3419ec9fdcd8dc7e64a2082744f
Deepanshu <deepanshusaxena315@gmail.com>
2025-05-31 23:36:04 +0530

    project and skills section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,9 @@ import StarBackground from '../components/StarBackground'
 import Navbar from '../components/Navbar'
 import HeroSection from '../components/HeroSection'
 import AboutSection from '../components/AboutSection'
+import SkillsSection from '../components/SkillsSection'
+import ProjectSection from '../components/ProjectSection'
+import ContactSection from '../components/ContactSection'
 
 const Home = () => {
   return (
@@ -15,6 +18,9 @@ const Home = () => {
       <main>
         <HeroSection/>
         <AboutSection/>
+        <SkillsSection/>
+        <ProjectSection/>
+        <ContactSection/>
       </main>
     </div>
   )
